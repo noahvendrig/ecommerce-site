7bd1c9aa31f4c0590cd583593f5791d8992a9e8e
sparkz11 <noahvendrig@gmail.com>
2021-10-04 02:59:53 +1100

    created app and added homepage files

---
+++ shopping-app/src/homepage.component.jsx
@@ -0,0 +1,38 @@
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Product</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Product</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Product</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Product</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className="title">Product</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+);
+export default HomePage;
