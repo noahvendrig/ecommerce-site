16594109789f48d27afb611439b960c165cf5d18
Noah Vendrig <noahvendrig@gmail.com>
2021-10-05 09:40:16 +1100

    made sign in buttons spread across sign in component

--- shopping-app/src/components/sign-in/sign-in.component.jsx
+++ shopping-app/src/components/sign-in/sign-in.component.jsx
@@ -39,9 +39,10 @@ class SignIn extends React.Component {
 
                     <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
                     <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
-
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    <div className='signIn-buttons-container'>
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
