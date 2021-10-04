c46d9fc6e04eabf7085994be02f8b139c6df1528
sparkz11 <noahvendrig@gmail.com>
2021-10-04 19:29:27 +1100

    update menu items to navigate withRouter

--- shopping-app/src/pages/homepage/homepage.component.jsx
+++ shopping-app/src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,9 @@
 import Directory from '../../components/directory/directory.component';
 
 
-const HomePage = ({ history }) => (
+const HomePage = () => (
     <div className="homepage">
-        <Directory history={history} />
+        <Directory />
     </div>
 
 );
