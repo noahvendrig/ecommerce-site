85442aaf1ead5ac09052db9f2e60b316462bb179
sparkz11 <noahvendrig@gmail.com>
2021-10-04 22:04:01 +1100

    collection preview

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -4,6 +4,8 @@ import './pages/homepage/homepage.styles.scss';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx'
+
 
 const HatsPage = () => (
   <div>
@@ -16,7 +18,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
