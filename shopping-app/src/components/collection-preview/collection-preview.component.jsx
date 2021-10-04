c65a874beb82c79274917c0f7dd882cbcd08291c
sparkz11 <noahvendrig@gmail.com>
2021-10-04 22:23:49 +1100

    shop preview

--- shopping-app/src/components/collection-preview/collection-preview.component.jsx
+++ shopping-app/src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import './collection-preview.styles.scss';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -7,8 +9,8 @@ const CollectionPreview = ({ title, items }) => (
         {
             items
             .filter((item, idx) => idx < 4)
-            .map((item) => (
-                <div key={item.id}>{item.name}</div>
+            .map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} {...otherItemProps}/>
             ))
         }
         </div>
