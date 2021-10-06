a624c6d41cd0d5bd7c99ca08dd318618d2799aad
Noah Vendrig <noahvendrig@gmail.com>
2021-10-06 15:44:55 +1100

    updated shop data and updated card utils to group items

--- shopping-app/src/redux/cart/cart.reducer.js
+++ shopping-app/src/redux/cart/cart.reducer.js
@@ -1,8 +1,11 @@
-import CartActionTypes from './cart.types'
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
+    cartItems: []
 };
+
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
@@ -10,9 +13,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
         default:
             return state;
     }
-}
+};
 
 export default cartReducer;
