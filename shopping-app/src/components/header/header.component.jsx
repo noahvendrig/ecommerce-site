8994be0b93d8d576ee6eadb3539c8443ce48f575
Noah Vendrig <noahvendrig@gmail.com>
2021-10-06 11:54:38 +1100

    cart dropdown can be toggled

--- shopping-app/src/components/header/header.component.jsx
+++ shopping-app/src/components/header/header.component.jsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -22,14 +25,19 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
 
     </div>
 
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
