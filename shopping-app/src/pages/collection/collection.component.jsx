05e236c1f9b5751d7c4e4b6523f3f91e32ed0000
sparkz11 <noahvendrig@gmail.com>
2021-10-09 17:57:21 +1100

    changed styling to vw instead of % on collection-item.styles

--- shopping-app/src/pages/collection/collection.component.jsx
+++ shopping-app/src/pages/collection/collection.component.jsx
@@ -5,12 +5,20 @@ import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 
-const CollectionPage = ({match}) => (
-    <div className='collection'>
-        <h2>collection</h2>
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+    <div className='collection-page'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+            ))}
 
-    </div>
-);
+        </div>
+
+    </div>)
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
