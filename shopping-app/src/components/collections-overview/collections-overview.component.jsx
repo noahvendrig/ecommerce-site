44dc38f4e5d6f1277ab36a83e50dc260f25bdd62
sparkz11 <noahvendrig@gmail.com>
2021-10-11 00:49:10 +1100

    FIXED SHOP PAGE, silly

--- shopping-app/src/components/collections-overview/collections-overview.component.jsx
+++ shopping-app/src/components/collections-overview/collections-overview.component.jsx
@@ -3,20 +3,20 @@ import './collections-overview.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
 
-const CollectionsOverview = ({collections}) => (
+const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps}/>
         ))}
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- shopping-app/src/redux/shop/shop.selectors.js
+++ shopping-app/src/redux/shop/shop.selectors.js
@@ -1,18 +1,19 @@
 import { createSelector } from 'reselect';
 
-import memoize from 'lodash.memoize';
-
-export const selectCollection = memoize((collectionUrlParam) => 
-    createSelector(
-        [selectCollections],
-        collections => 
-            collections[collectionUrlParam]
-    )
-);
-
 const selectShop = state => state.shop;
- 
+
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
